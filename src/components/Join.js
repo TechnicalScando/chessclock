04eeadba12bee3fcea67d38f691669ff752add0d
Gary <kikz270@gmail.com>
2020-08-06 19:12:15 -0500

    finished basic join room page

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 // import { StyleSheet, css } from 'aphrodite'
 
 // const styles = StyleSheet.create({
+
 // })
 
 const Join = () => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
   return (
-    <h1>Join Room</h1>
+    <div className='joinOuterContainer'>
+      <div className='joinInnterContainer'>
+        <h1 className='heading'>Join</h1>
+        <div><input placeholder='Name' className='joinInput' type='text' onChange={(event) => setName(event.target.value)} /></div>
+        <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(event) => setRoom(event.target.value)} /></div>
+        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button className='button mt-20' type='submit'>Sign In</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
