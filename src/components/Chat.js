fd3344f9ec8d9280da2e00a38d93599e0ed248f0
Gary <kikz270@gmail.com>
2020-08-16 18:45:28 -0500

    Added input bar to chat, adds messages to message list

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,6 +3,7 @@ import queryString from 'query-string'
 import io from 'socket.io-client'
 
 import InfoBar from './InfoBar'
+import Input from './Input'
 
 let socket
 
@@ -62,11 +63,7 @@ const Chat = ({ location }) => {
     <div>
       <div>
         <InfoBar room={room} />
-        {/* <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-        /> */}
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
   )
