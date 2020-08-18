6534e788330a2a2bb38ef98348e7b05ead78fd8a
Gary <kikz270@gmail.com>
2020-08-17 20:18:22 -0500

    messages now work in the chat box

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client'
 
 import InfoBar from './InfoBar'
 import Input from './Input'
+import Messages from './Messages'
 
 let socket
 
@@ -63,6 +64,7 @@ const Chat = ({ location }) => {
     <div>
       <div>
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
