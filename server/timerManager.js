b68c8ace4801272d7a16fab6aa37c21c7d937614
Gary <kikz270@gmail.com>
2020-10-02 17:52:58 -0500

    barebones timerManager

---
+++ server/timerManager.js
@@ -0,0 +1,23 @@
+const timerCount
+const selectedTimer
+const timers = []
+
+const runTimer = () => {
+
+}
+
+const stopTimer = () => {
+    
+}
+
+const switchTimer = () => {
+    
+}
+
+const resetTimer = () => {
+    
+}
+
+const formatTimer = () => {
+    
+}
